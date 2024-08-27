73f908e0c9cd088f7171bbed8c109550cf06214b
MER6 <sxsuzbek@gmail.com>
2024-08-27 08:38:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 from telethon import TelegramClient, events
 
-api_id = 'Укажите здесь API_ID'
-api_hash = 'Введите свои значения'
-phone_number = 'Введите свои значения'
+api_id = 'Укажите ваш API_ID'
+api_hash = 'Укажите ваш API_HASH'
+phone_number = 'Укажите ваш номер телефона'
 
 device_model = 'iPhone'
 system_version = '14.5'
@@ -11,7 +11,7 @@ app_version = '8.0.1'
 lang_code = 'en'
 system_lang_code = 'en'
 
-client = TelegramClient('session_name', api_id, api_hash,
+client = TelegramClient('account', api_id, api_hash,
                         device_model=device_model,
                         system_version=system_version,
                         app_version=app_version,
@@ -20,7 +20,7 @@ client = TelegramClient('session_name', api_id, api_hash,
 
 @client.on(events.NewMessage)
 async def handle_new_message(event):
-    if event.raw_text == 'бе':
+    if event.raw_text == 'test':
         await client.delete_messages('me', [event.message])
 
 async def main():
